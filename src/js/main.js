ef1b6e400fd1f9abae09f2b5d9b1ad0e2d13db5e
Oleg Moskalenko <oleg.moskalenko@gmail.com>
2019-03-28 16:42:37 +0200

    Добавлен функционал для слайдера

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
 (function () {
+  /*======   begin MENU   ======*/
   var mainNav = document.querySelector(".mainNav");
   var toggle = document.querySelector(".mainNav__toggle");
 
   mainNav.classList.remove('mainNav--nojs');
 
-  toggle.addEventListener("click", function(event) {
+  toggle.addEventListener("click", function (event) {
     event.preventDefault();
-    if(mainNav.classList.contains("mainNav--closed")) {
+    if (mainNav.classList.contains("mainNav--closed")) {
       mainNav.classList.remove("mainNav--closed");
       mainNav.classList.add("mainNav--opened");
     } else {
@@ -14,4 +15,41 @@
       mainNav.classList.add("mainNav--closed");
     }
   });
+  /*======    end MENU    ======*/
+
+  /*======   begin SLIDER   ======*/
+  var slider = document.querySelector(".slider__images");
+  var imgBefore = slider.querySelector(".slider__image--before");
+  var imgAfter = slider.querySelector(".slider__image--after");
+  var radioBefore = document.getElementById("before");
+  var radioAfter = document.getElementById("after");
+  var track = document.querySelector(".slider__track");
+  var thumb = track.querySelector(".slider__thumb");
+  imgBefore.style.width = "100%";
+  imgAfter.style.width = "0";
+
+  radioBefore.addEventListener("change", function(event) {
+    event.preventDefault();
+    if(radioBefore.checked == true) {
+      imgBefore.style.width = "100%";
+      imgAfter.style.width = "0";
+      thumb.style.marginLeft = "0";
+    }
+  });
+
+  radioAfter.addEventListener("change", function(event) {
+    event.preventDefault();
+    if(radioAfter.checked == true) {
+      imgAfter.style.width = "100%";
+      imgBefore.style.width = "0";
+      thumb.style.marginLeft = "35px";
+    }
+  });
+
+
+
+
+  /*======    end SLIDER    ======*/
+
+
 })();
